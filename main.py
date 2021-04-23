c941dfa7ef341dab65bd0e616cfe8692521ffd38
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2021-04-24 00:50:31 +0300

    delete code that does nothing

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter
 from module.models.point import Point
-from module.algo.jarvis import jarvis
-from PyQt5.QtWidgets import QApplication, QGraphicsView, QHBoxLayout, QListView, QVBoxLayout, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QGraphicsView, QHBoxLayout, QListView, QWidget
 from base.point import PointListModel, PointScene
+
 
 class PointGraphicsView(QGraphicsView):
     def __init__(self, parent=None):
         super(PointGraphicsView, self).__init__(parent)
-
-    def mouseDoubleClickEvent(self, event) -> None:
-        self.setDragMode(QGraphicsView.DragMode.NoDrag)
-        return super().mouseDoubleClickEvent(event)
-
-    def mousePressEvent(self, event):
         self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
         self.setRenderHint(QPainter.RenderHint.Antialiasing, True)
-        return super().mousePressEvent(event)
-    
-    def mouseReleaseEvent(self, event):
-        self.setDragMode(QGraphicsView.DragMode.NoDrag)
-        return super().mouseReleaseEvent(event)
-    
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
     def wheelEvent(self, event):
+        factor = 0.8
         if event.angleDelta().y() > 0:
-            factor = 1.25
+            self.scale(1/factor, 1/factor)
         else:
-            factor = 0.8
-        
-        self.scale(factor, factor)
+            self.scale(factor, factor)
             
 
 class Main(QWidget):
@@ -36,23 +27,12 @@ class Main(QWidget):
         model = PointListModel([Point(1,1), Point(100,100)])
         listView = QListView()
         listView.setModel(model)
-        
-        def onClick(scene):
-            scene.constructConvexHull(jarvis)
-
-        hullButton = QPushButton("Construct convex hull")
 
         layout = QHBoxLayout()
-        v_layout = QVBoxLayout()
-        v_layout.addWidget(listView)
-        scene = PointScene(model)
-
-        hullButton.clicked.connect(lambda: onClick(scene))
-        v_layout.addWidget(hullButton)
-        layout.addLayout(v_layout)
+        layout.addWidget(listView)
 
         view = PointGraphicsView()
-        view.setScene(scene)
+        view.setScene(PointScene(model))
         layout.addWidget(view)
         self.setLayout(layout)
 
