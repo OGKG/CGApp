f9900fc5a153c31bdfac7b17aa831f6211a5ba26
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2021-04-27 23:45:20 +0300

    provide loci method interactive mode

--- algo/loci.py
+++ algo/loci.py
@@ -8,23 +8,29 @@ class LociGraphicsItem(PointGraphicsItem):
     def append_children(self):
         self.hline = QGraphicsLineItem(QLineF(QPointF(0, 0), QPointF(1000, 0)), parent=self)
         self.vline = QGraphicsLineItem(QLineF(QPointF(0, 0), QPointF(0, 1000)), parent=self)
-        self.text = QGraphicsTextItem(parent=self)
 
     def setText(self, text):
         self.text.setPlainText(text)
-
-    def refresh(self, loci):
-        self.setText(str(loci.query(self.point)))
 
 
 class LociPointScene(PointScene):
     graphicsItemClass = LociGraphicsItem
 
+    def __init__(self, point_model):
+        super().__init__(point_model)
+        self.textFields = []
+
     def refresh(self):
+        self.clearTextFields()
         self.loci = Loci()
         self.loci.append_points(*self.point_model.points)
-        for point in self.items():
-            if isinstance(point, LociGraphicsItem):
-                point.refresh(self.loci)
+        for key, value in self.loci.repr.items():
+            text = QGraphicsTextItem(str(value))
+            text.moveBy(*key.coords)
+            self.textFields.append(text)
+            self.addItem(text)
 
+    def clearTextFields(self):
+        for textField in self.textFields:
+            self.removeItem(textField)
 
